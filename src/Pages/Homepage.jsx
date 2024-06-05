3fa9b9489d793509baff0ee5e5584c8e5c3a157c
Oyama <godstimeoyama082@gmail.com>
2024-06-05 11:59:08 -0500

    working on homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,7 +2,7 @@ import "../cssStyles/homePage.css"
 import { Link } from "react-router-dom"
 import Cards from "../components/Cards"
 import DataIcon from "../components/DataIcon"
-
+import HappyCard from "../components/HappyCard"
 export default function Homepage(){
     return (
       <>
@@ -61,22 +61,25 @@ export default function Homepage(){
       img="tractorIcon.svg" 
       text="Agricultural           
           Equipment"
+        width={12} 
           /> 
       <Cards 
       img="labourersIcon.svg" 
       text="Farm Hands"
+      width={12} 
       /> 
       <Cards 
       img="landIcons.svg" 
       text=" Fertile Lands Available
             To Farmers"
+      width={20}     
       /> 
       </div>
 
       {/* DATA ICONS SECTION */}
       <div className="data--icons">
         {/*  */}
-        <DataIcon img="achieveIcon.svg"  number='80+' text="Achievements"/>
+        <DataIcon img="achieveIcon.svg"   number='80+' text="Achievements"/>
         <DataIcon img="LabourIconDark.svg"  number='250+' text="Farmers"/>
         <DataIcon img="comunityIcon.svg"  number='100+' text="Community"/>
         <DataIcon img="tractorDark.svg"  number='310+' text="Farm Equipment"/>
@@ -90,7 +93,7 @@ export default function Homepage(){
         <h1 className="title">
          Our Mission
         </h1>
-        <div className="">
+        <div className="title--text">
           <h1>Unleash The Power Of
              Modern Farming</h1>
           <p>To empower farmers with state of the 
@@ -108,6 +111,46 @@ export default function Homepage(){
         </figure>
        </div>
       </div>
+      {/* Happy Customers */}
+      <div className="happyCustomers">
+      <h1>Happy <span>Costumers</span></h1>
+      <div className="happy--cards--wrap">
+      <HappyCard 
+      img="happyCustomers-1.jpg"
+      name="Mathew Cole"
+       />
+      <HappyCard 
+      img="happyCustomers-2.jpg"
+      name="David Right"
+       />
+      <HappyCard 
+      img="happyCustomers-3.jpg"
+      name="Sara Martins"
+      />
+      </div>
+      </div>
+      {/* Get in touch section*/}
+      <div className="get--in--touch">
+        <div className="text-wraps">
+        <h1><span className="block">Get In Touch With Us -</span>
+         <span className="block">Your Agricultural</span>
+         Solutions Partner</h1>
+         <p>Thank you for considering us fot your Agricultural
+          needs . We are eager to assist you with our expertise
+         and services . Feel free to reach out.</p>
+         <Link>Contact us</Link>
+         </div>
+      </div>
+      {/* Footer section */}
+       <footer className="footerSection">
+        <div>
+          <figure>
+            logo
+          </figure>
+          <p>Our Communities</p>
+          <p>Help</p>
+        </div>
+       </footer>
 
       </>
             
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 
-export default function Cards({img , text}){
+export default function Cards({img , text, width}){
     return(
         <div className="agric--Equip">
         <figure>
          <img src={img} alt="" />
         </figure>
-        <h4>{text}</h4>
+        <h4 style={{width:`${width}rem`}}>{text}</h4>
        </div>
     )
 }
