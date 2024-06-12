8f0769c39b68cbdea5ede5b51b8519f8c34d9e02
Oyama <godstimeoyama082@gmail.com>
2024-06-12 08:48:53 +0800

    updated..

--- src/components/SignUpClientForm.jsx
+++ src/components/SignUpClientForm.jsx
@@ -5,26 +5,28 @@ import { useNavigate } from 'react-router-dom';
 function SignUpClientForm() {
   const [formData, setFormData] = useState({
     name: '',
-    phoneNumber: '',
     email: '',
     password: '',
     confirmPassword: '',
+    contactNumber: '',
+    role: "client"
   });
+
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
 
-  const handleChange = (e) => {
+  function handleChange(e) {
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    });
-  };
+    })
+  }
 
-  const validateEmail = (email) => {
+  function validateEmail (email){
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(String(email).toLowerCase());
-  };
+  }
 
   const handleBlur = (e) => {
     const { name, value } = e.target;
@@ -41,12 +43,12 @@ function SignUpClientForm() {
     }
   };
 
-  const handleSubmit = async (e) => {
+  async function handleSubmit(e){
     e.preventDefault();
-    const { name, phoneNumber, email, password, confirmPassword } = formData;
+    const { name,  contactNumber, email, password, confirmPassword } = formData;
 
     // Basic validation
-    if (!name || !phoneNumber || !email || !password || !confirmPassword) {
+    if (!name || ! contactNumber || !email || !password || !confirmPassword) {
       setErrors((prevErrors) => ({
         ...prevErrors,
         form: 'All fields are required',
@@ -64,22 +66,36 @@ function SignUpClientForm() {
       setErrors((prevErrors) => ({
         ...prevErrors,
         confirmPassword: 'Passwords do not match',
-      }));
+      }
+    ));
       return;
+    }else{
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            confirmPassword: '',
+          }
+        ));
     }
 
     // Sending POST request to the server
     try {
-      const response = await fetch('https://example.com/api/signup', {
+      const response = await fetch('https://enoma-backend-v1.onrender.com/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
+        mode: "cors"
       });
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        // throw new Error('Network response was not ok')
+        console.log("error")
+        console.log(formData)
+        console.log(response)
+        
+
+          
       }
 
       const data = await response.json();
@@ -100,32 +116,54 @@ function SignUpClientForm() {
         ...prevErrors,
         form: 'Failed to sign up',
       }));
-      console.error('There was an error!', error);
+      console.error('There was an error!', error)
     }
-  };
+  }
+// toggle States
+  const [toggle1, setToggle1] = useState(false)
+  const [toggle2, setToggle2] = useState(false)
+
+  function passwordToggle1(){
+    setToggle1(preVState=>!preVState)
+    
+  }
+  function passwordToggle2(){
+    setToggle2(preVState=>!preVState)
+  }
+   
+
+    // JSX RETURN
 
   return (
     <form onSubmit={handleSubmit} className="signUpForm">
-      {errors.form && <p style={{ color: 'red' }}>{errors.form}</p>}
+      {errors.form && <p style={{ color: 'red' ,fontSize: "1rem" }}>{errors.form}</p>}
+      {/* hidden input role */}
+      <input 
+      type="hidden"
+       name="role"
+       value={formData.role} 
+      
+       />
+      {/* hidden input role */}
       <label>Name</label>
       <input
         type="text"
         name="name"
         value={formData.name}
         onChange={handleChange}
-        required
+        
       />
-      {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
+      {errors.name && <p style={{  color: 'red' ,fontSize: "1rem"}}>{errors.name}</p>}
 
       <label>Phone number</label>
       <input
         type="number"
-        name="phoneNumber"
-        value={formData.phoneNumber}
+        name="contactNumber"
+        value={formData.contactNumber}
         onChange={handleChange}
-        required
+    
       />
-      {errors.phoneNumber && <p style={{ color: 'red' }}>{errors.phoneNumber}</p>}
+      {errors.phoneNumber && <p style={{  color: 'red' ,fontSize: "1rem"}}>{errors.phoneNumber}</p>}
 
       <label>Email</label>
       <input
@@ -134,29 +172,34 @@ function SignUpClientForm() {
         value={formData.email}
         onChange={handleChange}
         onBlur={handleBlur}
-        required
+        
       />
-      {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
+      {errors.email && <p style={{  color: 'red' ,fontSize: "1rem"}}>{errors.email}</p>}
 
       <label>Password</label>
       <input
-        type="password"
+        type={toggle1? "text" : "password"}
         name="password"
         value={formData.password}
         onChange={handleChange}
-        required
+        
       />
-      {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
+      {/* Toogle 1 */}
+       <span className="toggle" style={{display: "block"} } onClick={passwordToggle1}>&#x1f441;</span>
+
+      {errors.password && <p style={{  color: 'red' ,fontSize: "1rem"}}>{errors.password}</p>}
 
       <label>Confirm Password</label>
       <input
-        type="password"
+        type={toggle2? "text" : "password"}
         name="confirmPassword"
         value={formData.confirmPassword}
         onChange={handleChange}
-        required
+        
       />
-      {errors.confirmPassword && <p style={{ color: 'red' }}>{errors.confirmPassword}</p>}
+      {/* Toggle 2 */}
+      <span className='toggle' style={{display: "block"}} onClick={passwordToggle2}>&#x1f441;</span>
+      {errors.confirmPassword && <p style={{  color: 'red' ,fontSize: "1rem"}}>{errors.confirmPassword}</p>}
 
       <button className="sign-in-button" type="submit">Sign Up</button>
     </form>
